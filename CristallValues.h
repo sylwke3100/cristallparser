2a534abee3596b88d536429023b1335d1200b6da
Sylwek <administrator@bercik.(none)>
2013-05-08 20:12:14 +0200

    Create unstable branch

---
+++ CristallValues.h
@@ -0,0 +1,18 @@
+#ifndef CRISTALLVALUES_H
+#define CRISTALLVALUES_H
+#include <map>
+#include <iostream>
+#include <cctype>
+#include <map>
+using namespace std;
+
+class CristallValues
+{
+    public:
+
+    protected:
+         map < int, map < const char, string > > Summary;
+    private:
+};
+
+#endif // CRISTALLVALUES_H
