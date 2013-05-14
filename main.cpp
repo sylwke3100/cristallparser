e7ee1763d21efa9018e79eeb67cb825005926f2b
KrzaQ <git@debil.eu>
2013-05-15 00:10:26 +0200

    Default main from the project.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-
+#include <iostream>
+#include "src/CristallParser.h"
+#include <vector>
+#include <map>
+using namespace std;
 
 int main()
 {
-
+	map <int, map<string,string > > c;
+	map < int, map < const char, string > > Dd;
+	CristallParser D;
+	D.addRule("numbers", CristallRuleNumbers, CristallNoLimit);
+	D.addRule("word",CristallRuleWord, CristallNoLimit);
+	D.setData("    Test word number 2555   ");
+	Dd = D.run();
+	for(int i =0; i<Dd.size(); i++)
+	{
+		cout<<Dd[i]['Label']<<" "<<Dd[i]['Value']<<endl;
+	}
+	return 0;
 }
