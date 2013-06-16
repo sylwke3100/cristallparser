59e27d800291aef76409fc13cf278de1e1982616
Sylwke3100 <sylwek3100@gmail,com>
2013-06-16 14:09:08 +0200

    Add group mamager as CristallGroup class

---
+++ CristallGroup.cpp
@@ -0,0 +1,27 @@
+#include "CristallGroup.h"
+
+
+void CristallGroup::addGroup(string GroupName)
+{
+    if(this->checkGroup(GroupName)==false)
+        GroupList.push_back(GroupName);
+}
+
+string CristallGroup::getGroup(int GroupID)
+{
+    if (GroupID>-1 and GroupID<GroupList.size())
+        return GroupList[GroupID];
+    else
+        return "";
+}
+
+int CristallGroup::checkGroup(string GroupName)
+{
+    int Is = false;
+    vector<string>::iterator element;
+    for(element = GroupList.begin(); element!=GroupList.end(); ++element)
+    {
+        if(*element==GroupName) return true;
+    }
+    return false;
+}
---
+++ CristallGroup.h
@@ -0,0 +1,21 @@
+#ifndef CRISTALLGROUP_H
+#define CRISTALLGROUP_H
+
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <cctype>
+#include <map>
+using namespace std;
+class CristallGroup
+{
+public:
+    void addGroup(string GroupName);
+    string getGroup(int GroupID);
+protected:
+    vector <string> GroupList;
+private:
+    int checkGroup(string GroupName);
+};
+
+#endif // CRISTALLGROUP_H
